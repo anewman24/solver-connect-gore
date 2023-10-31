097bb21cafd7be06b232f0d7e0170cfbb4277947
Audrey Newman <audreyrnew@gmail.com>
2023-10-31 15:45:48 -0500

    Basic types and functions outlined

--- Solver.hs
+++ Solver.hs
@@ -1,7 +1,7 @@
 
 --Data
 data Color = Yellow | Red | Maybe Color
-
+data Game = undefined
 
 
 -- Type Aliases
@@ -9,4 +9,19 @@ data Color = Yellow | Red | Maybe Color
 type Column = [Color]
 type Board = [Column]
 type Move = (Integer, Color)
+type Winner = undefined
+
+
 -- Functions
+--
+findWinner :: Game -> Winner
+findWinner = undefined
+
+gameResult :: Game -> Move -> Game
+gameResult = undefined
+
+allowedMoves :: Game -> [Move]
+allowedMoves = undefined
+
+showBoard :: Game -> String
+showBoard = undefined
