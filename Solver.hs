f70bae4aa034069ab8ee9ebb71f090f9b65174bf
Audrey Newman <audreyrnew@gmail.com>
2023-10-31 16:05:03 -0500

    Updated types

--- Solver.hs
+++ Solver.hs
@@ -1,15 +1,20 @@
 
 --Data
-data Color = Yellow | Red | Maybe Color
-data Game = undefined
+data Color = Yellow | Red deriving (Show, Eq)
+data Winner = Tie | Winner Color deriving (Show, Eq)
+
 
 
 -- Type Aliases
 --
 type Column = [Color]
+
+--not sure if we want to just have a 
 type Board = [Column]
-type Move = (Integer, Color)
-type Winner = undefined
+type Move = Int
+
+-- Idea is to have board and which player's turn it is (maybe include number of moves?)
+type Game = (Board, Color)
 
 
 -- Functions
