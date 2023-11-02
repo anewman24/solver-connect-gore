3ed40029b66ccc5a0040b6301251d7002883a772
Mary margaret Cooney <marymargaretcooney@marys-air-4.trinity.local>
2023-11-02 18:00:43 -0500

    Not finished with FindWinner but made progress

--- Solver.hs
+++ Solver.hs
@@ -19,8 +19,45 @@ type Game = (Board, Color)
 
 -- Functions
 --
+verticalWin:: Color -> Column -> Bool
+verticalWin Red col = aux col 0
+    where aux _ 4 = True
+          aux [] num = False
+          aux (Red:xs) num = aux xs (num + 1)
+          aux (Yellow:xs) num = aux xs 0
+verticalWin Yellow col = aux col 0
+    where aux _ 4 = True
+          aux [] num = False
+          aux (Yellow:xs) num = aux xs (num + 1)
+          aux (Red:xs) num = aux xs 0
+
+
+
+horizonalWin :: Color -> Column -> Column -> Column -> Column-> Bool
+horizonalWin Red colone coltwo colthree colfour = aux colone coltwo colthree colfour 0
+    where aux colone coltwo colthree colfour 1 = True
+          aux _ _ _ _ num = False
+          
+
+
+
+
+
+digonalWin :: Color -> Board -> Bool
+digonalWin = undefined
+
+
 findWinner :: Game -> Maybe Winner
-findWinner = undefined
+findWinner _ = Nothing
+findWinner (board, currentPlayer) = undefined
+
+
+   
+    --case board of
+    --[] -> Just $ Win Yellow
+    --[]-> Just $ Win Red
+    --[] -> Just $ Tie
+
 
 updateBoard :: Game -> Move -> Game
 updateBoard = undefined
