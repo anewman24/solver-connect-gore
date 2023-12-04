70ce7a5ee9b17a4a966b5270854832f7648dbac0
Maci Flanagan <mflanaga@trinity.edu>
2023-12-04 13:11:10 -0600

    Update TestInputs.hs

--- TestInputs.hs
+++ TestInputs.hs
@@ -5,6 +5,10 @@ correctColumn = [Red,Yellow,Red,Red]
 tooBigCol = [Red,Yellow,Red,Red,Yellow,Red,Yellow]
 emptyBoard = [[]]
 
+--generic sample board
+sampleBoard = [[Red, Yellow], [Red,Red,Yellow,Yellow,Red], [], [Red,Red,Red,Yellow,Yellow,Yellow], [Red,Red,Red], [Yellow, Red, Red,Yellow], []]
+
+
 
 --if run findWinner will get Win Red
 redWin1 = ([[],[Yellow,Red,Red],[Yellow,Red,Red,Yellow],[Red,Yellow],[Yellow,Red,Red,Yellow],[Yellow,Red],[]], Yellow)
@@ -26,5 +30,14 @@ invalidGame1 = ([[Red],[Yellow],tooBigCol,[],[],[],[]], Red)
 -- has too many columns
 invalidGame2 = ([[Red],[Yellow],[Yellow,Red],[Red],[],[],[],[Red]], Red)
 
+
 --use to test bestMove
 bmBoard = [[Yellow,Yellow,Red,Yellow,Yellow],[Red,Red,Yellow,Red,Red,Yellow],[Yellow,Red,Red,Red,Yellow,Red],[Red,Red,Red,Yellow,Red,Yellow],[Yellow,Red,Yellow,Yellow,Red],[Yellow,Yellow,Red,Red,Red],[Yellow,Red,Yellow,Yellow,Yellow]]
+
+
+--use to test winAcross, Yellow should win
+diagWinYellowBoard = [[Yellow,Red,Red,Red],[Yellow,Red,Yellow,Red],[Yellow,Yellow,Red],[Red,Yellow,Yellow],[Yellow,Red,Yellow],[Yellow,Red],[Red,Yellow]]
+
+
+--use to test verticalWinBoard, Yellow should win
+tallWinYellowBoard = [[Red,Red,Red],[Yellow,Red],[Red,Red],[Red,Yellow],[Yellow,Red,Yellow],[Yellow,Yellow,Yellow,Yellow,Red],[Red,Yellow,Yellow,Yellow]]
